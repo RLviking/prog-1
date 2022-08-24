5171f80bcbb4042e49cbbc8522c0b7a6b816ca98
RLviking <din.email@ga.ntig.se>
2022-08-24 11:14:57 +0200

    lektion 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello world")
+username = "Rasmus"
+print (username)
+while True:
+    name = input("vill du bli ihop?")
+    print (name)
+    print ("du svarade", name,)
+    svar = input("är du säker")
+    if svar == "ja" or "Ja":
+        break
+print ("tack för ditt svar")
