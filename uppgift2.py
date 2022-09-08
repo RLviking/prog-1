dd38ff10ec0ec0147737c0f096fe0e0a54f47a55
RLviking <din.email@ga.ntig.se>
2022-09-08 09:12:02 +0200

    hampus är bäst

---
+++ uppgift2.py
@@ -0,0 +1,26 @@
+import random
+
+computer = random.choice(["rock", "paper", "scissors"])
+
+user = input("rock, paper or scissors? ")
+
+print("The computer chose", computer,"and the user chose", user +".")
+
+# TODO - Implement the if statements that prints who wins
+if user == "rock" and computer == "scissors":
+    print ("win")
+
+elif user == "paper" and computer == "rock":
+    print ("win")
+
+elif user == "scissors" and computer == "paper":
+    print ("win")
+
+elif user == computer:
+    print ("draw")
+
+else:
+    print("lose")
+
+    
+
