57a9ee0ad611b06ee32a9ca464de9659bfefd834
RLviking <din.email@ga.ntig.se>
2022-10-17 13:05:13 +0200

    skottår

---
+++ skottår.py
@@ -0,0 +1,9 @@
+while True:
+    år = int(input("skriv ett år"))
+    if år % 4 == 0:
+        print("det är ett skottår")
+    else:
+        print( år, "är icke ett skottår, skriv ett nytt tal")
+        
+
+
